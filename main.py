a8e0167c5c5cac725d7ba2eb0e57df82f81254d3
Jose Marchini <jmarchinisarmiento@gmail.com>
2023-02-01 14:59:11 +0100

    Add new DB railway

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ app = Flask(__name__, static_folder='templates')
 
 cors=CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'containers-us-west-147.railway.app'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Exitosox100pre'
-app.config['MYSQL_DB'] = 'pagepersonal'
+app.config['MYSQL_PASSWORD'] = 'nk9zeeEQucaPJTOm9OJs'
+app.config['MYSQL_DB'] = 'railway'    
+app.config['MYSQL_PORT'] = '7091'
+app.config['MYSQL_URL'] = 'mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}'
 mysql = MySQL(app)
 
 
